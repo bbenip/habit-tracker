d80ff23a811fe96b012fc3389ebc0e48edf17f67
Baljinder Benipal <bbenipal10@gmail.com>
2023-09-18 21:56:14 -0400

    Add completed status to habits
    - Enable completing habit with checkbox
    - Use <del> to show completed habits
    - Move add button above habit list

--- src/HabitList.tsx
+++ src/HabitList.tsx
@@ -1,41 +1,92 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useCallback, useMemo, useState } from 'react';
+
+type Habit = {
+  id: string;
+  isCompleted: boolean;
+  name: string;
+};
+
+const createNewHabit = (): Habit => ({
+  id: crypto.randomUUID(),
+  isCompleted: false,
+  name: '',
+});
 
 export const HabitList = () => {
-  const [habits, setHabits] = useState<string[]>([]);
+  const [habits, setHabits] = useState<Habit[]>([]);
   const [isAddNewHabit, setIsAddNewHabit] = useState(false);
 
-  const [newHabit, setNewHabit] = useState('');
+  const [newHabit, setNewHabit] = useState<Habit>(createNewHabit());
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const setNewHabitName = useCallback((name: string) => {
+    setNewHabit((currentNewHabit) => ({ ...currentNewHabit, name }));
+  }, []);
+
+  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setHabits((currentHabits) => [...currentHabits, newHabit]);
     setIsAddNewHabit(false);
 
-    setNewHabit('');
-  };
+    setNewHabit(createNewHabit());
+  }, [createNewHabit, newHabit]);
 
-  const habitTags = habits.map((habit) => (
-    <li>
-      <label htmlFor={habit}>{habit}</label>
-    </li>
-  ));
+  const habitTags = useMemo(() => (
+    habits.map(({ id, isCompleted, name }) => (
+      <li key={id}>
+        <input
+          type="checkbox"
+          id={id}
+          name={name}
+          checked={isCompleted}
+          onChange={(e) => setHabits((currentHabits) => {
+            const newHabits = [...currentHabits];
+            const habitToModify = newHabits.find(
+              ({ id: habitId }) => id === habitId
+            )!;
+
+            habitToModify.isCompleted = e.target.checked;
+
+            return newHabits;
+          })}
+        />
+        {isCompleted ? (
+          <del><label htmlFor={id}>{name}</label></del>
+        ) : (
+          <label htmlFor={id}>{name}</label>
+        )}
+      </li>
+    ))
+  ), [habits]);
 
   return (
     <>
       <h1>Habits</h1>
-      <ul>{habitTags}</ul>
       {isAddNewHabit ? (
         <>
           <form onSubmit={handleSubmit}>
-            <input type="text" id="habit" required maxLength={50} value={newHabit} onChange={(e) => setNewHabit(e.target.value)} />
+            <input
+              type="text"
+              id="habit"
+              required
+              maxLength={50}
+              value={newHabit.name}
+              onChange={(e) => setNewHabitName(e.target.value)}
+              autoFocus
+            />
             <button type="submit">Submit</button>
           </form>
 
-          <button type="button" onClick={() => setIsAddNewHabit(false)}>Cancel</button>
+          <button
+            type="button"
+            onClick={() => setIsAddNewHabit(false)}
+          >
+            Cancel
+          </button>
         </>
       ) : (
         <button onClick={() => setIsAddNewHabit(true)}>Add habit</button>
       )}
+      <ul>{habitTags}</ul>
     </>
   );
 }
