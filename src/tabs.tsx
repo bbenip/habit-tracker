cf49f11ccfd8c6f91c0cf62f92e0559842a93379
Baljinder Benipal <bbenipal10@gmail.com>
2023-09-27 14:59:19 -0400

    Style tab list
    - Style active and hover states of tabs
    - Style content container with min/max width

--- src/tabs.tsx
+++ src/tabs.tsx
@@ -17,19 +17,24 @@ export const Tabs = ({ tabs }: TabsProps) => {
   // The list does not change
   // So, index is an acceptable key
   const navigationList = useMemo(() => (
-    tabs.map(({ title }, index) => (
-      <li key={index}>
-        <button onClick={() => setActiveIndex(index)}>{title}</button>
-      </li>
-    ))
-  ), [tabs]);
+    tabs.map(({ title }, index) => {
+      const activeStyle = index === activeIndex ? styles.active : '';
+      const listItemStyle = [styles.tab, activeStyle].join(' ');
+
+      return (
+        <li className={listItemStyle} key={index}>
+          <button onClick={() => setActiveIndex(index)}>{title}</button>
+        </li>
+      );
+    })
+  ), [activeIndex, tabs]);
 
   return (
-    <>
-      <nav>
-        <ol className={styles.tabList}>{navigationList}</ol>
+    <div className={styles.container}>
+      <nav className={styles.navigation}>
+        <ul className={styles.tabList}>{navigationList}</ul>
       </nav>
       {currentComponent}
-    </>
+    </div>
   );
 }
